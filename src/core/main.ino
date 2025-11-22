268c97cfc678ba00f462f450d068dde7c6c9c982
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:21:07 +0100

    Document main program structure in main.ino
    Added comments to explain the main program structure and functions.

--- src/core/main.ino
+++ src/core/main.ino
@@ -1,1 +1,31 @@
+/*
+main.ino
 
+Rolle: Einstiegspunkt des Programms.
+
+Inhalt:
+
+setup() und loop()
+
+Ruft Initialisierungsfunktionen aus system_init auf
+
+Nutzt die öffentlichen Schnittstellen der anderen Module, aber enthält keine Sensorlogik im Detail
+*/
+
+#include "system_init.h"
+#include "menu_system.h"
+#include "display.h"
+#include "buttons.h"
+
+void setup() {
+  systemInit();
+}
+
+void loop() {
+  updateButtons();
+  updateSensors();
+  updateNavigation();
+  updateMenuSystem();
+  renderDisplay();
+  handleAlarms();
+}
