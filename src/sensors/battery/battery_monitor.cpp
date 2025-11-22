0cb348979fca07beec894778b1e870b5bdc0844f
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:48:07 +0100

    Update battery_monitor.cpp

--- src/sensors/battery/battery_monitor.cpp
+++ src/sensors/battery/battery_monitor.cpp
@@ -1,1 +1,11 @@
+/*
+Rolle: Akku-Spannungsmessung.
 
+Inhalt:
+
+Lesen eines ADC-Pins
+
+Umrechnung auf reale Spannung (Spannungsteiler, Referenzspannung)
+
+Berechnung eines groben Ladezustands in %
+*/
