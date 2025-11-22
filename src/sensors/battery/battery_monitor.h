cf38ec6613f1bb5745ed64cd4b7140b7e4ae33a7
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:47:57 +0100

    Add documentation for battery_monitor.h
    Added documentation for battery voltage measurement functionality.

--- src/sensors/battery/battery_monitor.h
+++ src/sensors/battery/battery_monitor.h
@@ -1,1 +1,18 @@
+/*
+Rolle: Akku-Spannungsmessung.
+
+Inhalt:
+
+Lesen eines ADC-Pins
+
+Umrechnung auf reale Spannung (Spannungsteiler, Referenzspannung)
+
+Berechnung eines groben Ladezustands in %
+*/
+
+namespace BatteryMonitor {
+  void begin();
+  void update();
+  BatteryStatus getStatus();
+}
 
