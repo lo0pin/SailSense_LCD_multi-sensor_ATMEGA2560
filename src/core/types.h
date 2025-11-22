f9f96090ec5512999dc450400dbb4f9192cfff55
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:26:30 +0100

    Define common structs for sensor data
    Added common data types and structs for environmental and motion data.

--- src/core/types.h
+++ src/core/types.h
@@ -1,1 +1,32 @@
+/*
+Rolle: Gemeinsame Datentypen und Structs.
+
+Inhalt:
+
+Structs für Messwerte und Zustände
+*/
+
+
+#pragma once
+
+struct EnvData {
+  float temperature;
+  float humidity;
+  float pressure;
+};
+
+struct MotionData {
+  float roll;
+  float pitch;
+  float yaw;
+};
+
+struct HeadingData {
+  float headingDeg;
+};
+
+struct BatteryStatus {
+  float voltage;
+  float percentage;
+};
 
