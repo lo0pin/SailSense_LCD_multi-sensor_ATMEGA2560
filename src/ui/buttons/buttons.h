83539b5bd6ddf7608de2ac9e1c57612702318edd
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 14:31:23 +0100

    Define button event types and structure
    Added documentation and button event structure.

--- src/ui/buttons/buttons.h
+++ src/ui/buttons/buttons.h
@@ -1,1 +1,27 @@
+/*
+Rolle: Verwaltung der 4 Taster.
+
+Inhalt:
+
+Pin-Definitionen (oder inkl. config.h)
+
+Entprellung
+
+Erkennung von Klick, Long-Press, Repeat
+
+Event-Interface für Menüsystem
+*/
+
+enum class ButtonEventType { None, ShortPress, LongPress };
+
+struct ButtonEvent {
+  uint8_t buttonId;
+  ButtonEventType type;
+};
+
+namespace Buttons {
+  void begin();
+  void update();
+  bool getNextEvent(ButtonEvent& ev);
+}
 
