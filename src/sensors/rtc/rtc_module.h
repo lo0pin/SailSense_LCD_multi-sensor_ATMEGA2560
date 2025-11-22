8ad4c75b8fb3cf7a3df1e799fb644e02135fe356
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:47:08 +0100

    Create rtc_module.h for RTC functionality
    Added a header file for RTC module with structure and namespace.

--- src/sensors/rtc/rtc_module.h
+++ src/sensors/rtc/rtc_module.h
@@ -1,1 +1,23 @@
+/*
+Rolle: Echtzeituhr.
+
+Inhalt:
+
+Wrapper um RTC-Library (DS3231/DS1307)
+
+getTime(), setTime(), evtl. getUnixTime()
+
+Abstraktion, damit der Rest des Codes keine RTC-Library direkt kennt
+*/
+
+struct DateTimeSimple {
+  uint16_t year;
+  uint8_t  month, day, hour, minute, second;
+};
+
+namespace RTCModule {
+  bool begin();
+  DateTimeSimple now();
+}
+
 
