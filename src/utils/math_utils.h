4463591691962becc89fbb1aa9ed5d0875f5ef96
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 14:36:18 +0100

    Add math utility functions in math_utils.h
    Added mathematical utility functions for angle normalization and interpolation.

--- src/utils/math_utils.h
+++ src/utils/math_utils.h
@@ -1,1 +1,20 @@
+/*
+Rolle: Mathematische Hilfsfunktionen.
+
+Inhalt:
+
+Winkel-Normalisierung (wrapAngle360, wrapAngle180)
+
+Umrechnungen (z. B. mbar → hPa, Grad ↔ rad)
+
+Interpolation, Mapping, evtl. einfache Statistik
+*/
+
+#pragma once
+
+namespace MathUtils {
+  float wrapAngle360(float deg);
+  float wrapAngle180(float deg);
+  float lerp(float a, float b, float t);
+}
 
