40686b58893e55f8e3ec648342dfb06534a9d190
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:47:18 +0100

    Add header comment for RTC module
    Add documentation for RTC module functionality and purpose.

--- src/sensors/rtc/rtc_module.cpp
+++ src/sensors/rtc/rtc_module.cpp
@@ -1,1 +1,11 @@
+/*
+Rolle: Echtzeituhr.
 
+Inhalt:
+
+Wrapper um RTC-Library (DS3231/DS1307)
+
+getTime(), setTime(), evtl. getUnixTime()
+
+Abstraktion, damit der Rest des Codes keine RTC-Library direkt kennt
+*/
