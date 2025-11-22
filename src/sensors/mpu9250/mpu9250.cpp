5b95f369660bf614090043cdfbd7c6064031f2a9
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:32:59 +0100

    Add comments for MPU9250 IMU initialization and usage
    Added initial comments for MPU9250 IMU functionality.

---
+++ src/sensors/mpu9250/mpu9250.cpp
@@ -0,0 +1,11 @@
+/*
+Rolle: IMU (Gyro/Accel) für Gier-/Roll-/Nickwinkel.
+
+Inhalt:
+
+Initialisierung des MPU6050
+
+Rohwerte holen, filtern
+
+ggf. Orientation-Berechnung (oder Übergabe der Rohdaten an navigation/motion)
+*/
