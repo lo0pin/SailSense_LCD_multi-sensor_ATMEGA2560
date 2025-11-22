86624672147b3addea0432b1d6fa693be81e3b16
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:22:58 +0100

    Add documentation for system initialization functions
    Added header comments detailing initialization functions and components.

--- src/core/system_init.h
+++ src/core/system_init.h
@@ -1,1 +1,32 @@
+/*
+Rolle: Alle Initialisierungen an einem Ort.
+
+Inhalt:
+
+Funktionen wie void systemInit();
+
+Initialisierung von:
+
+Serial (optional)
+
+I2C (Wire.begin())
+
+Sensoren (BME280, MPU6050, GY-271, RTC)
+
+Display
+
+Buttons
+
+Buzzer
+
+globale Zustandsobjekte
+*/
+
+// system_init.h
+#pragma once
+void systemInit();
+void updateSensors();
+void updateNavigation();
+void handleAlarms();
+
 
