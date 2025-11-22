cf710025b3edec72c881844ce2cb820300575df1
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:29:15 +0100

    Define BME280Sensor namespace and functions
    Added documentation and function declarations for BME280 sensor.

--- src/sensors/bme280/bme280_sensor.h
+++ src/sensors/bme280/bme280_sensor.h
@@ -1,1 +1,21 @@
+/*
+Rolle: Kapselt alles rund um den BME280.
+
+Inhalt:
+
+Klasse oder Namespace wie BME280Sensor
+
+begin(), update(), getData()
+
+Interner Umgang mit der verwendeten BME-Library (Adafruit o. ä.)
+*/
+
+#pragma once
+#include "types.h"
+
+namespace BME280Sensor {
+  bool begin();
+  void update();
+  EnvData getEnvData();
+}
 
