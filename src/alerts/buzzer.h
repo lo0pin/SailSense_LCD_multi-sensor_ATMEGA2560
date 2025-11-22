72b3502a6cee7bd6e5427f865a25db54db3c41e8
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 14:33:41 +0100

    Create buzzer.h for acoustic signals and alarms
    Added header file for Buzzer functionality including methods for beeping and alarms.

--- src/alerts/buzzer.h
+++ src/alerts/buzzer.h
@@ -1,1 +1,25 @@
+/*
+Rolle: Akustische Signale & Alarme.
+
+Inhalt:
+
+Abstraktion für:
+
+kurzer Beep (Button-Feedback)
+
+Pattern für Alarm
+
+evtl. non-blocking Tonsteuerung über millis()
+*/
+
+#pragma once
+
+namespace Buzzer {
+  void begin();
+  void beepOk();
+  void beepError();
+  void alarmOn();
+  void alarmOff();
+  void update();   // für zeitbasierte Tonmuster
+}
 
