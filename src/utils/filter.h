82b9c929eb0c35601f835599e5066ce5b0ab7a84
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 14:35:27 +0100

    Create filter.h for general filter functionalities
    Added a header file for reusable filter classes and functions.

--- src/utils/filter.h
+++ src/utils/filter.h
@@ -1,1 +1,24 @@
+/*
+Rolle: Allgemeine Filter (wiederverwendbar).
+
+Inhalt:
+
+Klassen/Funktionen für:
+
+gleitender Mittelwert
+
+Exponential Moving Average
+
+ggf. einfachen 1. Ordnung Lowpass
+*/
+
+#pragma once
+
+class MovingAverage {
+public:
+  MovingAverage(size_t size);
+  float update(float value);
+private:
+  // Buffer, Index, Sum etc.
+};
 
