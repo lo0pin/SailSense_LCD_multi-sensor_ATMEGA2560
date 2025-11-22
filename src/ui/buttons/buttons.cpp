b658fff2279e6f511b32fc035e46acd7d9e05931
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 14:30:56 +0100

    Add comments for button management in buttons.cpp
    Added comments for button management functionality.

--- src/ui/buttons/buttons.cpp
+++ src/ui/buttons/buttons.cpp
@@ -1,1 +1,13 @@
+/*
+Rolle: Verwaltung der 4 Taster.
 
+Inhalt:
+
+Pin-Definitionen (oder inkl. config.h)
+
+Entprellung
+
+Erkennung von Klick, Long-Press, Repeat
+
+Event-Interface für Menüsystem
+*/
