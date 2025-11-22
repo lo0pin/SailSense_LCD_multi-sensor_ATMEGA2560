55cd797e2f211742564e0f2cdef710eb6647e04f
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:32:42 +0100

    Add MPU9250 sensor header file with interface
    This header file defines the MPU9250 sensor interface, including initialization, data update, and raw motion data retrieval functions.

---
+++ src/sensors/mpu9250/mpu9250.h
@@ -0,0 +1,17 @@
+/*
+Rolle: IMU (Gyro/Accel) für Gier-/Roll-/Nickwinkel.
+
+Inhalt:
+
+Initialisierung des MPU6050
+
+Rohwerte holen, filtern
+
+ggf. Orientation-Berechnung (oder Übergabe der Rohdaten an navigation/motion)
+*/
+
+namespace MPU9250Sensor {
+  bool begin();
+  void update();
+  MotionData getMotionDataRaw();  // oder getrennt accel/gyro
+}
