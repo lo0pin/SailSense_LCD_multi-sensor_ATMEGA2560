1f86690526e7a008812527e2cbc6082fe3d5ef48
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-22 12:29:27 +0100

    Document BME280 sensor functionality
    Added documentation comment for BME280 sensor class.

--- src/sensors/bme280/bme280_sensor.cpp
+++ src/sensors/bme280/bme280_sensor.cpp
@@ -1,1 +1,11 @@
+/*
+Rolle: Kapselt alles rund um den BME280.
 
+Inhalt:
+
+Klasse oder Namespace wie BME280Sensor
+
+begin(), update(), getData()
+
+Interner Umgang mit der verwendeten BME-Library (Adafruit o. ä.)
+*/
