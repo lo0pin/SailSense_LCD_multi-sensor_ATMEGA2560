e2091e548920e556a02e4297b05fd27c1e4d4426
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-23 13:12:23 +0100

    Add initial system functions to testfile.cpp

--- code_test/testfile.cpp
+++ code_test/testfile.cpp
@@ -1,1 +1,30 @@
+#include "testfile.h"
 
+
+void systemInit(){
+  
+}
+
+void updateButtons(){
+  
+}
+
+void updateSensors(){
+  
+}
+
+void updateNavigation(){
+  
+}
+
+void updateMenuSystem(){
+  
+}
+
+void renderDisplay(){
+  
+}
+
+void handleAlarms(){
+  
+}
