bbaa42bc52ede4f57a5b6da837c5f454eb0ff781
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-23 13:07:58 +0100

    Add main setup and loop functions for Arduino

--- code_test/main.ino
+++ code_test/main.ino
@@ -1,1 +1,14 @@
+#include testfile.h
 
+void setup(){
+  systemInit();
+}
+
+void loop(){
+  updateButtons();
+  updateSensors();
+  updateNavigation();
+  updateMenuSystem();
+  renderDisplay();
+  handleAlarms();
+}
