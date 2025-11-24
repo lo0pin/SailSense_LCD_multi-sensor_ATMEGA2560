f02eb98bdb9e404b075261844bc42a695ab65a44
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-24 19:50:04 +0100

    Update testfile.h

--- code_test/testfile.h
+++ code_test/testfile.h
@@ -1,4 +1,7 @@
-void systemInit();
+class Adafruit_BME280;
+
+void systemInit(Adafruit_BME280& bme_var);
+bool initBME280(Adafruit_BME280& bme_var);
 void updateButtons();
 void updateSensors();
 void updateNavigation();
