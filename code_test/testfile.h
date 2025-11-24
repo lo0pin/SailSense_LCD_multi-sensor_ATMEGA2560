c58c25410c0914fd14de3ed2d734507630348773
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-24 20:47:26 +0100

    Add OLED display initialization to systemInit

--- code_test/testfile.h
+++ code_test/testfile.h
@@ -1,9 +1,19 @@
+constexpr uint8_t  SCREEN_WIDTH    128
+constexpr uint8_t  SCREEN_HEIGHT   64
+constexpr int8_t   OLED_RESET      -1      // Reset-Pin (oder -1, wenn über Arduino-Reset)
+constexpr uint8_t  SCREEN_ADDRESS  0x3C    // I2C-Adresse deines Displays
+
+constexpr uint8_t MPU9250_ADDR      0x68
+//constexpr uint8_t INT_PIN           2          // optional, falls INT verbunden ist
+
 class Adafruit_BME280;
 class RTC_DS3231;
+class Adafruit_SSD1306;
 
-void systemInit(Adafruit_BME280& bme_var, RTC_DS3231& rtc_var);
+void systemInit(Adafruit_BME280& bme_var, RTC_DS3231& rtc_var, Adafruit_SSD1306& oled_var);
 bool initBME280(Adafruit_BME280& bme_var);
 bool initRTC(RTC_DS3231& rtc_var);
+bool initDISPLAY(Adafruit_SSD1306& display_var);
 
 void updateButtons();
 void updateSensors();
