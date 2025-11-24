8ec31b0326de149068c16d626389ed603bdbbdc6
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-24 20:02:40 +0100

    Update systemInit and add RTC initialization

--- code_test/testfile.h
+++ code_test/testfile.h
@@ -1,7 +1,10 @@
 class Adafruit_BME280;
+class RTC_DS3231;
 
-void systemInit(Adafruit_BME280& bme_var);
+void systemInit(Adafruit_BME280& bme_var, RTC_DS3231& rtc_var);
 bool initBME280(Adafruit_BME280& bme_var);
+bool initRTC(RTC_DS3231& rtc_var);
+
 void updateButtons();
 void updateSensors();
 void updateNavigation();
