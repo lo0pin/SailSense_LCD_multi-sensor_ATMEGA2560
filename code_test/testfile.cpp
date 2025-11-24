d50551f834dbb8f0695cdd108279ca066a9c02d4
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-24 19:48:41 +0100

    Modify systemInit and initBME280 for BME280 setup
    Refactor systemInit to accept Adafruit_BME280 reference and add debugging support.

--- code_test/testfile.cpp
+++ code_test/testfile.cpp
@@ -1,9 +1,50 @@
 #include "testfile.h"
 
+#include <Wire.h>
 
-void systemInit(){
-  
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define DEBUG 0
+
+void systemInit(Adafruit_BME280& bme_var, ){
+#IF DEBUG
+  Serial.begin(9600);
+  while (!Serial) { /* warten, falls nötig */ }
+#ENDIF
+  wire.begin();
+  while(!initBME280(bme_var)){
+#IF DEBUG
+  Serial.println("BME nicht gefunden")
+  delay(1000);
+#ENDIF  
+  }
 }
+
+
+bool initBME280(Adafruit_BME280& bme_var) {
+  // Erst Adresse 0x76 versuchen
+  if (bme_var.begin(0x76)) {
+#IF DEBUG
+    Serial.println(F("BME280 gefunden auf Adresse 0x76."));
+#ENDIF
+    return true;
+  }
+  // Dann 0x77
+  if (bme_var.begin(0x77)) {
+#IF DEBUG
+    Serial.println(F("BME280 gefunden auf Adresse 0x77."));
+#ENDIF
+    return true;
+  }
+
+  return false;
+}
+
+
 
 void updateButtons(){
   
