1b82a0e4a8460144f309ce20210c661fd993dc06
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-25 09:52:46 +0100

    Refactor includes and class declarations in testfile.h

--- code_test/testfile.h
+++ code_test/testfile.h
@@ -1,5 +1,15 @@
 #pragma once
 
+// Bibliotheken einbinden, damit die Typen vollständig sind wenn main.ino
+// die globalen Objekte (z.B. Adafruit_BME280 bme;) deklariert.
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include "RTClib.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <MPU9250_WE.h>
+
 constexpr uint8_t  SCREEN_WIDTH =    128;
 constexpr uint8_t  SCREEN_HEIGHT =   64;
 constexpr int8_t   OLED_RESET =      -1;      // Reset-Pin (oder -1, wenn über Arduino-Reset)
@@ -8,11 +18,7 @@ constexpr uint8_t  SCREEN_ADDRESS =  0x3C;    // I2C-Adresse deines Displays
 constexpr uint8_t MPU9250_ADDR =      0x68;
 //constexpr uint8_t INT_PIN           2          // optional, falls INT verbunden ist
 
-class Adafruit_BME280;
-class RTC_DS3231;
-class Adafruit_SSD1306;
-class MPU9250_WE;
-
+// Funktionsdeklarationen
 void systemInit(Adafruit_BME280& bme_var, RTC_DS3231& rtc_var, Adafruit_SSD1306& oled_var, MPU9250_WE& imu_var);
 bool initBME280(Adafruit_BME280& bme_var);
 bool initRTC(RTC_DS3231& rtc_var);
