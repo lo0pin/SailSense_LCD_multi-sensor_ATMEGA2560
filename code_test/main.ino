e9f261d479a2f72bea2ff874151d701050d223fc
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-26 21:39:19 +0100

    Refactor main.ino to improve structure and readability

--- code_test/main.ino
+++ code_test/main.ino
@@ -1,204 +1,44 @@
-/////////////////////////////////////////
 #include "testfile.h"
 
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#include "RTClib.h"
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <MPU9250_WE.h>
-/////////////////////////////////////////
 
+Adafruit_BME280     bme; // I2C
+RTC_DS3231          rtc;
+Adafruit_SSD1306    display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+MPU9250_WE imu =    MPU9250_WE(MPU9250_ADDR);
 
-#define initialize_delay 20
-#define initialize_fail_delay 1000
-
-uint16_t delaytime = 300;
-unsigned long globaltimer =0;
-uint16_t minite = 0;
-
-
-/////////////////////////////////////////
-
-void systemInit(Adafruit_BME280& bme_var, RTC_DS3231& rtc_var, Adafruit_SSD1306& display_var, MPU9250_WE& imu_var){
-#if DEBUG
-  Serial.begin(9600);
-  delay(initialize_delay);
-  // Hinweis: on Mega/2560 ist while(!Serial) normalerweise nicht nötig und kann blockieren.
-  while (!Serial) { /* warten, falls nötig */ }
-#endif
-  Wire.begin();
-  Wire.setClock(400000);   
-  delay(initialize_delay);
-  while(!initBME280(bme_var)){
-#if DEBUG
-    Serial.println("BME-Sensor nicht gefunden");
-#endif 
-    delay(initialize_fail_delay);
-  }
-#if DEBUG
-    Serial.println("BME-Sensor initialisiert!");
-#endif 
-  delay(initialize_delay);
-  while(!initRTC(rtc_var)){
-#if DEBUG
-    Serial.println(F("FEHLER: DS3231-RTC nicht gefunden. Wiring/Adresse prüfen!"));
-#endif 
-    delay(initialize_fail_delay);
-  }
-#if DEBUG
-    Serial.println(F("DS3231-RTC initialisiert!"));
-#endif 
-  delay(initialize_delay);
-  while(!initDISPLAY(display_var)){
-#if DEBUG    
-    Serial.println(F("SSD1306-Display nicht gefunden. Check Verkabelung/Adresse!"));
-#endif 
-    delay(initialize_fail_delay);
-  }
-  delay(initialize_delay);
-#if DEBUG    
-    Serial.println(F("SSD1306-Display initialisiert!"));
-#endif 
-  while(!initIMU(imu_var)){
-#if DEBUG    
-    Serial.println(F("FEHLER: MPU9250-Sensor antwortet nicht. Verkabelung/Adresse prüfen!"));
-#endif   
-    delay(initialize_fail_delay);
-  }
-#if DEBUG    
-    Serial.println(F("MPU9250-Sensor initialisiert!"));
-#endif     
-/*
-*/  
-  //Tasterpins initialisieren
-  for (int i = 8; i<=12; ++i){
-    pinMode(i, INPUT_PULLUP);
-  }
-#if DEBUG    
-  Serial.println(F("Tasterpins initialisiert!"));
-#endif 
+void setup() {
+  systemInit(bme, rtc, display, imu);
 }
 
-bool initBME280(Adafruit_BME280& bme_var) {
-  // Erst Adresse 0x76 versuchen
-  if (bme_var.begin(0x76)) {
-#if DEBUG
-    Serial.println(F("BME280 gefunden auf Adresse 0x76."));
-#endif
-    return true;
+void loop() {
+  DateTime right_now = rtc.now();
+  BMEData current_bme;
+  BMEData mittelw_bme;
+  uint8_t buttoninput;
+  if (millis() - globaltimer > delaytime) {
+    buttoninput = updateButtons();
+
+    current_bme = updateSensors(bme);
+    
+    /*
+    mittelw_bme.temp += current_bme.temp;
+    mittelw_bme.humi += current_bme.humi;
+    mittelw_bme.baro += current_bme.baro;
+    
+    mittelw_bme.temp /= 2;
+    mittelw_bme.humi /= 2;
+    mittelw_bme.baro /= 2;
+     */
+
+    
+    
+    globaltimer = millis();
+
   }
-  // Dann 0x77
-  if (bme_var.begin(0x77)) {
-#if DEBUG
-    Serial.println(F("BME280 gefunden auf Adresse 0x77."));
-#endif
-    return true;
-  }
-
-  return false;
-}
-
-bool initRTC(RTC_DS3231& rtc_var){
-  if(rtc_var.begin()){
-    if (rtc_var.lostPower()) {
-#if DEBUG
-      Serial.println(F("RTC meldet Stromverlust → Zeit wird neu gesetzt."));
-#endif  
-      rtc_var.adjust(DateTime(__DATE__, __TIME__));
-    } else {
-#if DEBUG
-      Serial.println(F("RTC hatte keinen Stromverlust (Zeit gültig)."));
-#endif
-    }
-#if SETTIMEONCE    
-    rtc_var.adjust(DateTime(__DATE__, __TIME__));
-    Serial.println(F("RTC auf Compile-Time gesetzt!"));
-#endif
-    return true;
-  }
-  return false;
-}
-
-bool initDISPLAY(Adafruit_SSD1306& display_var){
-  if(display_var.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)){
-    display_var.clearDisplay();
-    display_var.setTextSize(1);
-    display_var.setTextColor(SSD1306_WHITE);
-    display_var.setCursor(0, 0);
-    return true;
-  }
-  return false;
-}
-
-bool initIMU(MPU9250_WE& imu_var){
-  if (imu_var.init()){
-    // ---------- Sensorbereiche setzen ----------
-    // Acc: +/-2g,4g,8g,16g
-    imu_var.setAccRange(MPU9250_ACC_RANGE_8G);     // guter Allround-Bereich
-    // Gyro: +/-250,500,1000,2000 deg/s
-    imu_var.setGyrRange(MPU9250_GYRO_RANGE_500);   // ausreichend dynamisch
-    // Mag: full-scale wird intern gesetzt; wir wählen Betriebsmodus:
-    imu_var.setMagOpMode(AK8963_CONT_MODE_100HZ);  // kontinuierlich, 100 Hz
-    return true;
-  }
-  return false;
-}
-
-//////////////////////////////////
 
 
-
-uint8_t updateButtons(){
-  for (uint8_t i= 8; i<=12; ++i){
-    if (digitalRead(i)==1){
-      return i;
-    }
-  }
-}
-
-//////////////////////////////////
-
-
-BMEData updateSensors(Adafruit_BME280& bme_var){
-  BMEData m;
-  m.temp = bme_var.readTemperature();         // °C
-  m.humi = bme_var.readHumidity();            // %
-  m.baro = bme_var.readPressure() / 100;    // hPa   
-  return m;       
-}
-
-//////////////////////////////////
-
-void updateNavigation(){
-  
-}
-
-//////////////////////////////////
-
-void updateMenuSystem(uint8_t button){
-  
-}
-
-void renderDisplay(Adafruit_SSD1306& dis, BMEData& bme_struct){
-  dis.clearDisplay();
-  dis.setCursor(0, 0);
-  
-  dis.print(F("T:   "));
-  dis.println(bme_struct.temp,1);
-  dis.print(F("H:   "));
-  dis.println(bme_struct.humi,1);
-  dis.print(F("P:   "));
-  dis.println(bme_struct.baro,1);
-
-  
-
-  dis.display();
-}
-
-//////////////////////////////////
-
-void handleAlarms(){
-  
+  updateNavigation();
+  updateMenuSystem(buttoninput);
+  renderDisplay(display, current_bme);
+  handleAlarms();
 }
