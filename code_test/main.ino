01f3bb7f89275321d69c383e274f01672829a6b5
Julian Kampitsch <136756500+lo0pin@users.noreply.github.com>
2025-11-29 17:11:12 +0100

    Optimize sensor updates and data handling in loop
    Refactor loop function to optimize sensor updates and handle hourly data storage.

--- code_test/main.ino
+++ code_test/main.ino
@@ -14,53 +14,66 @@ void setup() {
 
 void loop() {
   DateTime right_now = rtc.now();
-  renderDisplay_everyLoop(display);
+  //renderDisplay_everyLoop(display);
   BMEData current_bme;
-  BMEData mittelw_bme;
   IMUData current_imu;
 
-  
+
   if (millis() - globaltimer > delaytime_for_loop) {
     buttoninput = updateButtons();
+    if (counter_for_measurment_within_loop % 2 == 1){
+      current_bme = updateSensors(bme);
+      counter_for_measurment_within_loop = 0;
+    } else {
+      counter_for_measurment_within_loop++;
+    }
+    
+    current_imu = updateNavigation(imu);
 
-    current_bme = updateSensors(bme);
-    current_imu = updateNavigation(imu); 
-    current_display = updateMenuSystem(buttoninput); 
-    renderDisplay(display, current_bme, current_imu, right_now, current_display); 
-    /*
-    mittelw_bme.temp += current_bme.temp;
-    mittelw_bme.humi += current_bme.humi;
-    mittelw_bme.baro += current_bme.baro;
-    
-    mittelw_bme.temp /= 2;
-    mittelw_bme.humi /= 2;
-    mittelw_bme.baro /= 2;
-     */
-    
+    current_display = updateMenuSystem(buttoninput);
+
+
+    if (right_now.minute() != old_minute) {
+      mittelw_bme = get_mittelwert(current_bme);
+      old_minute = right_now.minute();
+    }
+    uint8_t right_now_hour = right_now.hour();
+    if (right_now_hour != old_hour) {
+      if (old_hour == 99) {
+        temp_messungen[right_now_hour] = mittelw_bme.temp;
+        humid_messungen[right_now_hour] = mittelw_bme.humi;
+        baro_messungen[right_now_hour] = mittelw_bme.baro;
+        
+      } else {
+        uint8_t index_minus_one_hour = (right_now_hour + 24 - 1) % 24;
+        temp_messungen[index_minus_one_hour] = mittelw_bme.temp;
+        humid_messungen[index_minus_one_hour] = mittelw_bme.humi;
+        baro_messungen[index_minus_one_hour] = mittelw_bme.baro;
+      }
+
+      old_hour = right_now.hour();
+    }
+
+    renderDisplay(display, current_bme, current_imu, right_now, current_display);
+
+
 #if DEBUG
     Serial.print(F("current_bme\t"));
-    Serial.print(current_bme.temp);
+    Serial.print(mittelw_bme.temp);
     Serial.print(F("\t"));
-    Serial.print(current_bme.humi);  
+    Serial.print(mittelw_bme.humi);
     Serial.print(F("\t"));
-    Serial.println(current_bme.baro);  
-  
+    Serial.println(mittelw_bme.baro);
+
     Serial.print(F("current_imu\t"));
     Serial.print(current_imu.roll);
     Serial.print(F("\t"));
-    Serial.print(current_imu.pitch);  
+    Serial.print(current_imu.pitch);
     Serial.print(F("\t"));
-    Serial.println(current_imu.heading); 
-  
-#endif     
-    
+    Serial.println(current_imu.heading);
+#endif
     globaltimer = millis();
-
   }
-
-
   
-  
-
   handleAlarms();
 }
